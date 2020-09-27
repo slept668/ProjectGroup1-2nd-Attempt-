936339ae9feb948d5287046d83e8cd79cbe52413
Brian <oldhamb90@gmail.com>
2020-09-27 00:18:50 -0400

    Added array

--- Oldham.js
+++ Oldham.js
@@ -4,12 +4,17 @@ for(let i = 1000; i >= 0; i--) {
 
 for(let i = 0; i <= 1000; i++) {
     if(i = 2500) {
-        alert("A quarter of the way there!")
+        alert("A quarter of the way there!");
     }
     if(i = 5000) {
-        alert("Halfway there!")
+        alert("Halfway there!");
     }
     if(i = 10000) {
-        alert("The loop is done!")
+        alert("The loop is done!");
     }
 }
+
+var movieArray = ["You Don't Mess with the Zohan", "Happy Gilmore", "Billy Madison", 
+"The Waterboy", 
+"The Longest yard"];
+
