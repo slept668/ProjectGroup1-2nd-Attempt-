ad5b19897f0b6536009edc6e9b154c14bf204e8f
Brian <oldhamb90@gmail.com>
2020-09-27 00:04:08 -0400

    Logging 1000 to 0

--- Oldham.js
+++ Oldham.js
@@ -1,20 +1,3 @@
-console.log("Github makes me wanna jump out the window -Brian O.");
-
-let firstName = "Brian";
-let lastName = "Oldham";
-let age = 30;
-let ofAge = "I am legally able to drink in the states";
-let notOfAge = "It is illegal for me to drink in the states";
-let just21 = "I am just barely able to drink legally in the states";
-
-alert("My name is " + firstName + " " + lastName);
-
-alert("I am " + age + " years old");
-
-if(age > 21) {
-    alert(ofAge);
-} else if (age < 21) {
-    alert(notOfAge);
-} else if (age = 21) {
-    alert(just21);
+for(let i = 1000; i >= 0; i--) {
+    console.log(i);
 }
