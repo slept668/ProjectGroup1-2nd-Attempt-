be8281dc173bebee192bd54a31c5a0ec02453358
Brian <oldhamb90@gmail.com>
2020-10-03 19:20:27 -0400

    Fixed potato code and changed log to every even number

--- Oldham.js
+++ Oldham.js
@@ -1,15 +1,15 @@
-for(let i = 1000; i >= 0; i--) {
+for(let i = 1000; i > 0; i-=2) {
     console.log(i);
 }
 
-for(let i = 0; i <= 1000; i++) {
-    if(i = 2500) {
+for(let i = 0; i <= 10000; i++) {
+    if(i === 2500) {
         alert("A quarter of the way there!");
     }
-    if(i = 5000) {
+    if(i === 5000) {
         alert("Halfway there!");
     }
-    if(i = 10000) {
+    if(i === 10000) {
         alert("The loop is done!");
     }
 }
